c1b15f714bdefcda5c8dcc20c8ea19ab229cf1ec
Lucas901 <lucasaguiartostes@hotmail.com>
2020-09-06 14:41:05 -0300

    adicionadolista com todos os valores das linhas e colunas do dataset

--- plot_graficos.py
+++ plot_graficos.py
@@ -7,25 +7,31 @@ import seaborn as sns
 #definindo como df o arquivo que será utilizado para analisar os dados
 df = pd.read_csv('NFLX.csv')
 
-#todos os dados da segunda coluna do dataset
-x=df.iloc[:,1]
 
-#todos os dados da primeira coluna do dataset
-y=df.iloc[:,0]
+#todos os dados de todas as colunas do dataset
+lista=[]
+count = len(df.columns)
+
+for coluna in range(count):
+    lista.append(df.iloc[:,coluna])
+   
+
 #%%
 
 plt.subplot(1,3,3)
-
 #grafico linear
-plt.plot(x,y)
+plt.plot(lista[1],lista[0])
 plt.show()
 
+
 #grafico em barra
-sns.barplot(data=df,x=x,y=y)
+sns.barplot(data=df,x=lista[1],y=lista[0])
 plt.show()
 
 #grafico de pizza
-plt.pie(x, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.pie(lista[1], autopct='%1.1f%%', shadow=True, startangle=90)
 plt.axis("equal")
 plt.show()
+
+
 # %%
